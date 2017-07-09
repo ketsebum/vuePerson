92fd238d384309baa5ec392c33c27ab18e759268
Ketse <snunleys@gmail.com>
2017-07-09 17:25:03 -0400

    Updating proxy table for local testing with webpack and GAE running

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,18 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  proxy: {
-    '/api': {
-        target: 'http://localhost:8080',
-        secure: false,
-        changeOrigin: true
-      },
-    "/_ah" : {
-      "target": "http://localhost:8080",
-      "secure": false,
-      "loglevel": "debug",
-      "changeOrigin": true
-      }
-    }
+  NODE_ENV: '"development"'
 })
--- config/index.js
+++ config/index.js
@@ -27,7 +27,19 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8080',
+        secure: false,
+        changeOrigin: true
+      },
+      "/_ah" : {
+        "target": "http://localhost:8080",
+        "secure": false,
+        "loglevel": "debug",
+        "changeOrigin": true
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
